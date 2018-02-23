dda797d1693e1e4fe261eae31b250b10f43e4c15
Dan Ibanez <daibane@sandia.gov>
2018-02-23 13:32:47 -0700

    maybe done?

--- src/GraphTraverser.cc
+++ src/GraphTraverser.cc
@@ -207,6 +207,353 @@ void populateLocalPsiBoundKokkos(
   }
 }
 
+static
+KOKKOS_INLINE_FUNCTION
+void calcSourceKokkos(
+    const double volume, 
+    const double localSource[g_nVrtxPerCell][g_nMaxGroups],
+    double cellSource[4],
+    const UINT group) 
+{
+  double q0 = localSource[0][group];
+  double q1 = localSource[1][group];
+  double q2 = localSource[2][group];
+  double q3 = localSource[3][group];
+
+  cellSource[0] = volume / 20.0 * (2.0 * q0 + q1 + q2 + q3);
+  cellSource[1] = volume / 20.0 * (q0 + 2.0 * q1 + q2 + q3);
+  cellSource[2] = volume / 20.0 * (q0 + q1 + 2.0 * q2 + q3);
+  cellSource[3] = volume / 20.0 * (q0 + q1 + q2 + 2.0 * q3);
+}
+
+static
+KOKKOS_INLINE_FUNCTION
+void calcVolumeIntegralsKokkos(
+    const double volume, 
+    const double area[g_nFacePerCell],
+    const double sigmaTotal,
+    double matrix[g_nVrtxPerCell][g_nVrtxPerCell]) 
+{
+  matrix[0][0] = area[0] / 12.0 + 2.0 * sigmaTotal * volume / 20.0;
+  matrix[0][1] = area[0] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[0][2] = area[0] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[0][3] = area[0] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+
+  matrix[1][0] = area[1] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[1][1] = area[1] / 12.0 + 2.0 * sigmaTotal * volume / 20.0;
+  matrix[1][2] = area[1] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[1][3] = area[1] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+
+  matrix[2][0] = area[2] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[2][1] = area[2] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[2][2] = area[2] / 12.0 + 2.0 * sigmaTotal * volume / 20.0;
+  matrix[2][3] = area[2] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+
+  matrix[3][0] = area[3] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[3][1] = area[3] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[3][2] = area[3] / 12.0 + 1.0 * sigmaTotal * volume / 20.0;
+  matrix[3][3] = area[3] / 12.0 + 2.0 * sigmaTotal * volume / 20.0;
+}
+
+static
+KOKKOS_INLINE_FUNCTION
+void calcOutgoingFluxKokkos(
+    const double area[g_nFacePerCell],
+    double matrix[g_nVrtxPerCell][g_nVrtxPerCell])
+{
+  if (area[0] > 0) {
+    matrix[1][1] += 2.0 * area[0] / 12.0;
+    matrix[1][2] += 1.0 * area[0] / 12.0;
+    matrix[1][3] += 1.0 * area[0] / 12.0;
+
+    matrix[2][1] += 1.0 * area[0] / 12.0;
+    matrix[2][2] += 2.0 * area[0] / 12.0;
+    matrix[2][3] += 1.0 * area[0] / 12.0;
+
+    matrix[3][1] += 1.0 * area[0] / 12.0;
+    matrix[3][2] += 1.0 * area[0] / 12.0;
+    matrix[3][3] += 2.0 * area[0] / 12.0;
+  }
+
+  if (area[1] > 0) {
+    matrix[0][0] += 2.0 * area[1] / 12.0;
+    matrix[0][2] += 1.0 * area[1] / 12.0;
+    matrix[0][3] += 1.0 * area[1] / 12.0;
+
+    matrix[2][0] += 1.0 * area[1] / 12.0;
+    matrix[2][2] += 2.0 * area[1] / 12.0;
+    matrix[2][3] += 1.0 * area[1] / 12.0;
+
+    matrix[3][0] += 1.0 * area[1] / 12.0;
+    matrix[3][2] += 1.0 * area[1] / 12.0;
+    matrix[3][3] += 2.0 * area[1] / 12.0;
+  }
+
+  if (area[2] > 0) {
+    matrix[0][0] += 2.0 * area[2] / 12.0;
+    matrix[0][1] += 1.0 * area[2] / 12.0;
+    matrix[0][3] += 1.0 * area[2] / 12.0;
+
+    matrix[1][0] += 1.0 * area[2] / 12.0;
+    matrix[1][1] += 2.0 * area[2] / 12.0;
+    matrix[1][3] += 1.0 * area[2] / 12.0;
+
+    matrix[3][0] += 1.0 * area[2] / 12.0;
+    matrix[3][1] += 1.0 * area[2] / 12.0;
+    matrix[3][3] += 2.0 * area[2] / 12.0;
+  }
+
+  if (area[3] > 0) {
+    matrix[0][0] += 2.0 * area[3] / 12.0;
+    matrix[0][1] += 1.0 * area[3] / 12.0;
+    matrix[0][2] += 1.0 * area[3] / 12.0;
+
+    matrix[1][0] += 1.0 * area[3] / 12.0;
+    matrix[1][1] += 2.0 * area[3] / 12.0;
+    matrix[1][2] += 1.0 * area[3] / 12.0;
+
+    matrix[2][0] += 1.0 * area[3] / 12.0;
+    matrix[2][1] += 1.0 * area[3] / 12.0;
+    matrix[2][2] += 2.0 * area[3] / 12.0;
+  }
+}
+
+static
+KOKKOS_INLINE_FUNCTION
+void calcIncomingFluxKokkos(
+    const UINT cell, 
+    const double area[g_nFacePerCell],
+    const double localPsiBound[g_nVrtxPerFace][g_nFacePerCell][g_nMaxGroups],
+    double cellSource[g_nVrtxPerCell],
+    const UINT group,
+    device_mat3_t<UINT> const& cell_to_face_vertex)
+{
+  UINT faceVertex0, faceVertex1, faceVertex2, faceVertex3;
+  double psiNeighbor0, psiNeighbor1, psiNeighbor2, psiNeighbor3;
+
+  if (area[0] < 0) {
+    faceVertex1 = cell_to_face_vertex(cell, 0, 1);
+    faceVertex2 = cell_to_face_vertex(cell, 0, 2);
+    faceVertex3 = cell_to_face_vertex(cell, 0, 3);
+
+    psiNeighbor1 = localPsiBound[faceVertex1][0][group];
+    psiNeighbor2 = localPsiBound[faceVertex2][0][group];
+    psiNeighbor3 = localPsiBound[faceVertex3][0][group];
+
+    cellSource[1] -= 2.0 * area[0] / 12.0 * psiNeighbor1;
+    cellSource[1] -= 1.0 * area[0] / 12.0 * psiNeighbor2;
+    cellSource[1] -= 1.0 * area[0] / 12.0 * psiNeighbor3;
+
+    cellSource[2] -= 1.0 * area[0] / 12.0 * psiNeighbor1;
+    cellSource[2] -= 2.0 * area[0] / 12.0 * psiNeighbor2;
+    cellSource[2] -= 1.0 * area[0] / 12.0 * psiNeighbor3;
+
+    cellSource[3] -= 1.0 * area[0] / 12.0 * psiNeighbor1;
+    cellSource[3] -= 1.0 * area[0] / 12.0 * psiNeighbor2;
+    cellSource[3] -= 2.0 * area[0] / 12.0 * psiNeighbor3;
+  }
+
+  if (area[1] < 0) {
+    faceVertex0 = cell_to_face_vertex(cell, 1, 0);
+    faceVertex2 = cell_to_face_vertex(cell, 1, 2);
+    faceVertex3 = cell_to_face_vertex(cell, 1, 3);
+
+    psiNeighbor0 = localPsiBound[faceVertex0][1][group];
+    psiNeighbor2 = localPsiBound[faceVertex2][1][group];
+    psiNeighbor3 = localPsiBound[faceVertex3][1][group];
+
+    cellSource[0] -= 2.0 * area[1] / 12.0 * psiNeighbor0;
+    cellSource[0] -= 1.0 * area[1] / 12.0 * psiNeighbor2;
+    cellSource[0] -= 1.0 * area[1] / 12.0 * psiNeighbor3;
+
+    cellSource[2] -= 1.0 * area[1] / 12.0 * psiNeighbor0;
+    cellSource[2] -= 2.0 * area[1] / 12.0 * psiNeighbor2;
+    cellSource[2] -= 1.0 * area[1] / 12.0 * psiNeighbor3;
+
+    cellSource[3] -= 1.0 * area[1] / 12.0 * psiNeighbor0;
+    cellSource[3] -= 1.0 * area[1] / 12.0 * psiNeighbor2;
+    cellSource[3] -= 2.0 * area[1] / 12.0 * psiNeighbor3;
+  }
+
+  if (area[2] < 0) {
+    faceVertex0 = cell_to_face_vertex(cell, 2, 0);
+    faceVertex1 = cell_to_face_vertex(cell, 2, 1);
+    faceVertex3 = cell_to_face_vertex(cell, 2, 3);
+
+    psiNeighbor0 = localPsiBound[faceVertex0][2][group];
+    psiNeighbor1 = localPsiBound[faceVertex1][2][group];
+    psiNeighbor3 = localPsiBound[faceVertex3][2][group];
+
+    cellSource[0] -= 2.0 * area[2] / 12.0 * psiNeighbor0;
+    cellSource[0] -= 1.0 * area[2] / 12.0 * psiNeighbor1;
+    cellSource[0] -= 1.0 * area[2] / 12.0 * psiNeighbor3;
+
+    cellSource[1] -= 1.0 * area[2] / 12.0 * psiNeighbor0;
+    cellSource[1] -= 2.0 * area[2] / 12.0 * psiNeighbor1;
+    cellSource[1] -= 1.0 * area[2] / 12.0 * psiNeighbor3;
+
+    cellSource[3] -= 1.0 * area[2] / 12.0 * psiNeighbor0;
+    cellSource[3] -= 1.0 * area[2] / 12.0 * psiNeighbor1;
+    cellSource[3] -= 2.0 * area[2] / 12.0 * psiNeighbor3;
+  }
+
+  if (area[3] < 0) {
+    faceVertex0 = cell_to_face_vertex(cell, 3, 0);
+    faceVertex1 = cell_to_face_vertex(cell, 3, 1);
+    faceVertex2 = cell_to_face_vertex(cell, 3, 2);
+
+    psiNeighbor0 = localPsiBound[faceVertex0][3][group];
+    psiNeighbor1 = localPsiBound[faceVertex1][3][group];
+    psiNeighbor2 = localPsiBound[faceVertex2][3][group];
+
+    cellSource[0] -= 2.0 * area[3] / 12.0 * psiNeighbor0;
+    cellSource[0] -= 1.0 * area[3] / 12.0 * psiNeighbor1;
+    cellSource[0] -= 1.0 * area[3] / 12.0 * psiNeighbor2;
+
+    cellSource[1] -= 1.0 * area[3] / 12.0 * psiNeighbor0;
+    cellSource[1] -= 2.0 * area[3] / 12.0 * psiNeighbor1;
+    cellSource[1] -= 1.0 * area[3] / 12.0 * psiNeighbor2;
+
+    cellSource[2] -= 1.0 * area[3] / 12.0 * psiNeighbor0;
+    cellSource[2] -= 1.0 * area[3] / 12.0 * psiNeighbor1;
+    cellSource[2] -= 2.0 * area[3] / 12.0 * psiNeighbor2;
+  }
+}
+
+static 
+KOKKOS_INLINE_FUNCTION
+void gaussElim4Kokkos(double A[4][4], double b[4])
+{
+  double tmp;
+
+  // Normalize first row
+  tmp = 1.0/A[0][0];
+  A[0][0] = 1.0;
+  A[0][1] = A[0][1] * tmp;
+  A[0][2] = A[0][2] * tmp;
+  A[0][3] = A[0][3] * tmp;
+  b[0] = b[0] * tmp;
+
+  // Set column zero to 0.0
+  tmp = A[1][0];
+  A[1][0] = 0.0;
+  A[1][1] = A[1][1] - A[0][1] * tmp;
+  A[1][2] = A[1][2] - A[0][2] * tmp;
+  A[1][3] = A[1][3] - A[0][3] * tmp;
+  b[1] = b[1] - b[0] * tmp;   
+
+  tmp = A[2][0];
+  A[2][0] = 0.0;
+  A[2][1] = A[2][1] - A[0][1] * tmp;
+  A[2][2] = A[2][2] - A[0][2] * tmp;
+  A[2][3] = A[2][3] - A[0][3] * tmp;
+  b[2] = b[2] - b[0] * tmp;
+
+  tmp = A[3][0];
+  A[3][0] = 0.0;
+  A[3][1] = A[3][1] - A[0][1] * tmp;
+  A[3][2] = A[3][2] - A[0][2] * tmp;
+  A[3][3] = A[3][3] - A[0][3] * tmp;
+  b[3] = b[3] - b[0] * tmp;
+
+  // Normalize second row
+  tmp = 1.0/A[1][1];
+  A[1][1] = 1.0;
+  A[1][2] = A[1][2] * tmp;
+  A[1][3] = A[1][3] * tmp;
+  b[1] = b[1] * tmp;
+
+  // Set column one to 0.0
+  tmp = A[2][1];
+  A[2][1] = 0.0;
+  A[2][2] = A[2][2] - A[1][2] * tmp;
+  A[2][3] = A[2][3] - A[1][3] * tmp;
+  b[2] = b[2] - b[1] * tmp;
+
+  tmp = A[3][1];
+  A[3][1] = 0.0;
+  A[3][2] = A[3][2] - A[1][2] * tmp;
+  A[3][3] = A[3][3] - A[1][3] * tmp;
+  b[3] = b[3] - b[1] * tmp;
+
+  // Normalize third row
+  tmp = 1.0/A[2][2];
+  A[2][2] = 1.0;
+  A[2][3] = A[2][3] * tmp;
+  b[2] = b[2] * tmp;
+
+  // Set column two to 0.0
+  tmp = A[3][2];
+  A[3][2] = 0.0;
+  A[3][3] = A[3][3] - A[2][3] * tmp;
+  b[3] = b[3] - b[2] * tmp;
+
+  // Backward Solve
+  b[3] = b[3]/A[3][3];    
+  b[2] = b[2] - A[2][3]*b[3];
+  b[1] = b[1] - A[1][3]*b[3] - A[1][2]*b[2];
+  b[0] = b[0] - A[0][3]*b[3] - A[0][2]*b[2] - A[0][1]*b[1]; 
+} 
+
+static 
+KOKKOS_INLINE_FUNCTION
+void solveKokkos(
+    const UINT cell, 
+    const UINT angle, 
+    const double sigmaTotal,
+    const double localPsiBound[g_nVrtxPerFace][g_nFacePerCell][g_nMaxGroups],
+    const double localSource[g_nVrtxPerCell][g_nMaxGroups],
+    double localPsi[g_nVrtxPerCell][g_nMaxGroups],
+    device_mat1_t<double> const& cell_volume,
+    device_mat2_t<double> const& face_area,
+    device_mat3_t<double> const& omega_dot_n,
+    device_mat3_t<UINT> const& cell_to_face_vertex
+    )
+{
+  double volume, area[g_nFacePerCell];
+
+  // Get cell volume and face areas
+  volume = cell_volume(cell);
+
+  area[0] = face_area(cell, 0) * 
+    omega_dot_n(angle, cell, 0);
+  area[1] = face_area(cell, 1) * 
+    omega_dot_n(angle, cell, 1);
+  area[2] = face_area(cell, 2) * 
+    omega_dot_n(angle, cell, 2);
+  area[3] = face_area(cell, 3) * 
+    omega_dot_n(angle, cell, 3);
+
+  // Solve local transport problem for each group
+  for (UINT group = 0; group < g_nGroups; group++) {
+
+    double cellSource[g_nVrtxPerCell] = {0.0};
+    double matrix[g_nVrtxPerCell][g_nVrtxPerCell] = {0.0};
+    double solution[g_nVrtxPerCell];
+
+    // form local source term
+    calcSourceKokkos(volume, localSource, cellSource, group);
+
+    // form streaming-plus-collision portion of matrix
+    calcVolumeIntegralsKokkos(volume, area, sigmaTotal, matrix);
+
+    // form dependencies on incoming (outgoing) faces
+    calcOutgoingFluxKokkos(area, matrix);
+    calcIncomingFluxKokkos(
+        cell, area, localPsiBound, cellSource, group,
+        cell_to_face_vertex);
+
+    // solve matrix
+    for (UINT vertex = 0; vertex < g_nVrtxPerCell; ++vertex)
+      solution[vertex] = cellSource[vertex];
+    gaussElim4Kokkos(matrix, solution);
+
+    // put local solution onto global solution
+    for (UINT vertex = 0; vertex < g_nVrtxPerCell; ++vertex)
+      localPsi[vertex][group] = solution[vertex];
+  }
+}
+
 
 /*
     traverse
@@ -330,6 +677,21 @@ void GraphTraverser::traverse()
           host_mat1_t<double>(
               g_sigmaT.data(),
               g_sigmaT.size());
+        auto host_cell_volume =
+          host_mat1_t<double>(
+              g_tychoMesh->c_cellVolume.data(),
+              g_nCells);
+        auto host_face_area =
+          host_mat2_t<double>(
+              g_tychoMesh->c_faceArea.data(),
+              g_nCells,
+              g_nFacePerCell);
+        auto host_cell_to_face_vertex =
+          host_mat3_t<UINT>(
+              g_tychoMesh->c_cellToFaceVrtx.data(),
+              g_nCells,
+              g_nFacePerCell,
+              g_nVrtxPerFace);
         auto device_source =
           Kokkos::create_mirror_view_and_copy(
               device(), host_source);
@@ -357,6 +719,15 @@ void GraphTraverser::traverse()
         auto device_sigma_t = 
           Kokkos::create_mirror_view_and_copy(
               device(), host_sigma_t);
+        auto device_cell_volume = 
+          Kokkos::create_mirror_view_and_copy(
+              device(), host_cell_volume);
+        auto device_face_area = 
+          Kokkos::create_mirror_view_and_copy(
+              device(), host_face_area);
+        auto device_cell_to_face_vertex = 
+          Kokkos::create_mirror_view_and_copy(
+              device(), host_cell_to_face_vertex);
 
         //actually do graph traversal
         auto nCells = g_nCells;
@@ -388,8 +759,10 @@ void GraphTraverser::traverse()
               device_adj_proc, device_side);
 
           // Transport solve
-          solve(cell, angle, device_sigma_t(cell),
-              localPsiBound, localSource, localPsi);
+          solveKokkos(cell, angle, device_sigma_t(cell),
+              localPsiBound, localSource, localPsi,
+              device_cell_volume, device_face_area,
+              device_omega_dot_n, device_cell_to_face_vertex);
 
           // localPsi -> psi
           for (UINT group = 0; group < g_nGroups; group++) {
